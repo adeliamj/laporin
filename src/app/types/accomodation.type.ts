e798a80010fd253690155fc1a60a099db070c8ca
adeliamj <adelia.miftakul.2205356@students.um.ac.id>
2025-11-25 15:31:20 +0700

    fix: delete cache ts

--- src/app/types/accomodation.type.ts
+++
@@ -1,9 +0,0 @@
-import { StaticImport } from 'next/dist/shared/lib/get-img-props';
-
-export interface AccomodationType {
-  idAccomodation: number | string;
-  imageUrl: string | StaticImport;
-  titleAccomodation: string;
-  description: string;
-  bookingUrl?: string;
-}
