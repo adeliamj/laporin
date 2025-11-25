ff6a10b29cce99435e349ef3f9caec779186932d
atavada <ardhaardhanaputra@gmail.com>
2025-11-25 10:28:26 +0700

    chore: setup prisma

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,11 +1,32 @@
 // lib/db.ts
-import mysql from 'mysql2/promise';
+// import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'laporin',
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST || 'localhost',
+//   user: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASS || '',
+//   database: process.env.DB_NAME || 'laporin',
+// });
+
+// export default pool;
+
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
 });
 
-export default pool;
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
